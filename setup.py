b1e6b4aaad1b6df4b28c94666efe6ab985bc2cef
Kyle Hornberg <kyle.hornberg@gmail.com>
2018-12-27 14:43:50 -0600

    Add mimimal python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import json
+from distutils.core import setup
+
+
+def get_version():
+    with open('package.json', 'r') as f:
+        package = json.load(f)
+        return(package['dependencies']['@octokit/routes'])
+
+
+setup(
+    name='octokitpy-routes',
+    version=get_version(),
+    description='Routes for octokitpy',
+    long_description='Routes from the NPM published octokit routes',
+    author='Kyle Hornberg',
+    author_email='kyle.hornberg@gmail.com',
+    url='https://github.com/khornberg/routes',
+    packages=['routes'],
+)
