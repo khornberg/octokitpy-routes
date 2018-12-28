220bd74557aed40c4e05ed5a7e40d1068061c3a3
Kyle Hornberg <kyle.hornberg@gmail.com>
2018-12-28 11:57:24 -0600

    Add package data

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 setup(
     name='octokitpy-routes',
-    version='0.0.1',
+    version='0.0.2',
     description='Routes for octokitpy',
     long_description='Routes from the NPM published octokit routes',
     author='Kyle Hornberg',
     author_email='kyle.hornberg@gmail.com',
     url='https://github.com/khornberg/routes',
     packages=['routes'],
+    include_package_data=True,
 )
