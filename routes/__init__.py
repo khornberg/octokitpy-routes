46ce5a756c2d5c8b4e2ffb6c673e7afd768467e7
Kyle Hornberg <kyle.hornberg@gmail.com>
2018-12-28 16:33:14 -0600

    Load all available specs

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,5 +3,11 @@ import os
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-with open(os.path.join(__location__, 'index.json'), 'r') as f:
-    routes = json.load(f)
+
+def _load_spec(spec_name):
+    with open(os.path.join(__location__, spec_name, 'index.json'), 'r') as f:
+        return json.load(f)
+
+
+specs = [d.name for d in os.scandir(__location__) if d.is_dir() and not d.name == '__pycache__']
+specifications = {spec: _load_spec(spec) for spec in specs}
