fc3d1fcebad2e02a44b486d05865f5019568ef96
Kyle Hornberg <kyle.hornberg@gmail.com>
2019-01-02 13:15:04 -0600

    directory structure

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'routes'))
 
 
 def _load_spec(spec_name):
