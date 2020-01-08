8c11c93cb0f1f3dff240ddb8b968b8fecfdbfb58
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-01-08 15:23:05 -0600

    Version 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='octokitpy-routes',
-    version='1.0.0',
+    version='2.0.0',
     description='Routes for octokitpy',
     long_description='Routes from the NPM published octokit routes',
     author='Kyle Hornberg',
@@ -13,4 +13,20 @@ setup(
     url='https://github.com/khornberg/octokitpy-routes',
     packages=['routes'],
     include_package_data=True,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Utilities',
+    ]
 )
