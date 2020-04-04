88c4adf3f7938678642838b32d7aec376f3bea1d
Patrick Sodré <psodre@gmail.com>
2020-04-04 02:54:34 -0400

    Add test for checking webhook_names is available

--- test_routes.py
+++ test_routes.py
@@ -1,7 +1,13 @@
 import unittest
 from octokit_routes import specifications
+from octokit_routes import webhook_names
 
 
 class TestRoutes(unittest.TestCase):
     def test_can_import_and_get_verion(self):
         self.assertEqual(specifications['api.github.com']['info']['version'], '9.0.0')
+
+
+class TestWebhooks(unittest.TestCase):
+    def test_can_import_webhooks(self):
+        self.assertTrue("*" in webhook_names)
