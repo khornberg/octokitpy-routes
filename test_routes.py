b11a9d0aa02f8805bb1c5ca6044f92bc486a58d5
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-10-12 09:35:45 -0500

    black and isort

--- test_routes.py
+++ test_routes.py
@@ -1,16 +1,16 @@
 import unittest
-from octokit_routes import specifications
-from octokit_routes import webhook_names
+
+from octokit_routes import specifications, webhook_names
 
 
 class TestRoutes(unittest.TestCase):
     def test_can_import_and_get_verion(self):
-        self.assertEqual(specifications['api.github.com']['info']['version'], '9.0.0')
+        self.assertEqual(specifications["api.github.com"]["info"]["version"], "9.0.0")
 
 
 class TestWebhooks(unittest.TestCase):
     def test_can_import_webhooks(self):
         self.assertTrue("*" in webhook_names)
-        self.assertTrue('check_suite.completed' in webhook_names)
-        self.assertTrue('workflow_run.requested' in webhook_names)
-        self.assertTrue('push' in webhook_names)
+        self.assertTrue("check_suite.completed" in webhook_names)
+        self.assertTrue("workflow_run.requested" in webhook_names)
+        self.assertTrue("push" in webhook_names)
